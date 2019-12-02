ed05bec82eaaa0048beb694ea7c2f017e9934879
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-02 13:08:02 -0500

    change header method to set

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const index = require("./routes/index.js");
 
 const app = express();
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(index);
