e94cf397f8d1d47a6e9b2ea510b06d7264674733
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-27 13:41:05 -0500

    Cleaned up orphaned functions and re-established development server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const axios = require("axios");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index.js");
 
 const app = express();
+
 app.use(function (req, res, next) {
     res.set("Access-Control-Allow-Origin", "*");
     res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,24 +18,6 @@ app.use(index);
 const server = http.createServer(app);
 
 const io = socketIo(server);
-
-const getApiAndEmit = async socket => {
-    try {
-        //Recieve event from the button that was pressed
-        //Emit the same event to everyone but the original sender
-
-        //--------------------------------
-        // const res = await axios.get(
-        //     "https://api.darksky.net/forecast/54d9c9a176d27b6fec0daee17babafac/43.7695,11.2558"
-        // );
-        // socket.emit("FromAPI", res.data.currently.temperature)
-        //--------------------------------        
-    } catch (error) {
-        console.error(`Error: ${error.code}`);
-    }
-};
-
-// let interval;
 
 io.on("connection", socket => {
     console.log("New client connected");
